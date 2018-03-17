b9a18dc2f62b52625f6e43fcbcb8d3abedb5c367
Martin Münch <minarth@gmail.com>
2018-03-17 19:40:51 +0100

    Add controling of first page for converting. Change max_page_count to last_page.

--- pdf2image/pdf2image.py
+++ pdf2image/pdf2image.py
@@ -6,18 +6,19 @@ from subprocess import Popen, PIPE
 from PIL import Image
 from io import BytesIO
 
-def convert_from_path(pdf_path, dpi=200, output_folder=None, max_page_count=None, fmt='ppm'):
+def convert_from_path(pdf_path, dpi=200, output_folder=None, first_page=None, last_page=None, fmt='ppm'):
     """
         Description: Convert PDF to Image will throw whenever one of the condition is reached
         Parameters:
             pdf_path -> Path to the PDF that you want to convert
             dpi -> Image quality in DPI (default 200)
             output_folder -> Write the resulting images to a folder (instead of directly in memory)
-            max_page_count -> Max number of page to process before stopping
+            first_page -> First page to process
+            last_page -> Last page to process before stopping
             fmt -> Output image format
     """
 
-    args, parse_buffer_func = __build_command(['pdftoppm', '-r', str(dpi), pdf_path], output_folder, max_page_count, fmt)
+    args, parse_buffer_func = __build_command(['pdftoppm', '-r', str(dpi), pdf_path], output_folder, first_page, last_page, fmt)
 
     proc = Popen(args, stdout=PIPE, stderr=PIPE)
 
@@ -28,14 +29,15 @@ def convert_from_path(pdf_path, dpi=200, output_folder=None, max_page_count=None
     else:
         return parse_buffer_func(data)
 
-def convert_from_bytes(pdf_file, dpi=200, output_folder=None, max_page_count=None, fmt='ppm'):
+def convert_from_bytes(pdf_file, dpi=200, output_folder=None, first_page=None, last_page=None, fmt='ppm'):
     """
         Description: Convert PDF to Image will throw whenever one of the condition is reached
         Parameters:
             pdf_file -> Bytes representing the PDF file
             dpi -> Image quality in DPI
             output_folder -> Write the resulting images to a folder (instead of directly in memory)
-            max_page_count -> Max number of page to process before stopping
+            first_page -> First page to process
+            last_page -> Last page to process before stopping
             fmt -> Output image format
     """
 
@@ -43,9 +45,9 @@ def convert_from_bytes(pdf_file, dpi=200, output_folder=None, max_page_count=Non
         with tempfile.NamedTemporaryFile('wb') as f:
             f.write(pdf_file)
             f.flush()
-            return convert_from_path(f.name, dpi=dpi, output_folder=output_folder, max_page_count=max_page_count, fmt=fmt)
+            return convert_from_path(f.name, dpi=dpi, output_folder=output_folder, first_page=first_page, last_page=last_page, fmt=fmt)
 
-    args, parse_buffer_func = __build_command(['pdftoppm', '-r', str(dpi)], output_folder, max_page_count, fmt)
+    args, parse_buffer_func = __build_command(['pdftoppm', '-r', str(dpi)], output_folder, first_page, last_page, fmt)
 
     proc = Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 
@@ -55,9 +57,12 @@ def convert_from_bytes(pdf_file, dpi=200, output_folder=None, max_page_count=Non
 
     return parse_buffer_func(data)
 
-def __build_command(args, output_folder, max_page_count, fmt):
-    if max_page_count is not None:
-        args.extend(['-l', str(max_page_count)])
+def __build_command(args, output_folder, first_page, last_page, fmt):
+    if first_page is not None:
+        args.extend(['-f', str(first_page)])
+
+    if last_page is not None:
+        args.extend(['-l', str(last_page)])
 
     parsed_format, parse_buffer_func = __parse_format(fmt)
 
@@ -113,4 +118,3 @@ def __parse_buffer_to_png(data):
 
 def __load_from_output_folder(output_folder):
     return [Image.open(os.path.join(output_folder, f)) for f in sorted(os.listdir(output_folder))]
-
