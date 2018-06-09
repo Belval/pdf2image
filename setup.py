5a3384ab7ea5a77753b57bf98ece01ea76a7e8f4
Tobias Happ <tobias.happ@gmx.de>
2018-06-09 10:48:13 +0200

    Adds python 2.7 support

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
