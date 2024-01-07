a2db9032f8b4303031c6c54e3bf3e39a83a68df2
John-Schreiber <56258966+John-Schreiber@users.noreply.github.com>
2024-01-07 12:45:59 -0500

    Compatibility with pyright

--- pdf2image/__init__.py
+++ pdf2image/__init__.py
@@ -2,9 +2,7 @@
     __init__ of the pdf2image module
 """
 
-from .pdf2image import (
-    convert_from_bytes,
-    convert_from_path,
-    pdfinfo_from_bytes,
-    pdfinfo_from_path,
-)
+from .pdf2image import convert_from_bytes as convert_from_bytes
+from .pdf2image import convert_from_path as convert_from_path
+from .pdf2image import pdfinfo_from_bytes as pdfinfo_from_bytes
+from .pdf2image import pdfinfo_from_path as pdfinfo_from_path
